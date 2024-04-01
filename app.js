8035fd7c4cfe43b7c24ad8e39239f55f2e2dd51a
Mukesh Loganathan <mukeshloganathan12@gmail.com>
2024-04-01 08:13:54 -0400

    mostly done and very cool

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 const mongoDb = "mongodb+srv://mukeshloganathan12:5I8C6b6XKIenJZ9f@cluster0.xnpubn3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(mongoDb);
@@ -21,15 +22,52 @@ const User = mongoose.model(
   })
 );
 
+
+
 const app = express();
 app.set("views", __dirname);
 app.set("view engine", "ejs");
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username: username });
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        };
+        if (user.password !== password) {
+          return done(null, false, { message: "Incorrect password" });
+        };
+        return done(null, user);
+      } catch(err) {
+        return done(err);
+      };
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch(err) {
+      done(err);
+    };
+  });
+
+  
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => {
+    res.render("index", { user: req.user });
+  });
+  
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 
 app.post("/sign-up", async (req, res, next) => {
@@ -45,5 +83,21 @@ app.post("/sign-up", async (req, res, next) => {
     };
   });
   
+app.post(
+    "/log-in",
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/"
+    })
+  );
+  
+app.get("/log-out", (req, res, next) => {
+req.logout((err) => {
+    if (err) {
+    return next(err);
+    }
+    res.redirect("/");
+});
+});
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
