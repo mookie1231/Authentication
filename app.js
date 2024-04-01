d00e20bd469d96e89b2da32db3d3a5eddc19b476
Mukesh Loganathan <mukeshloganathan12@gmail.com>
2024-04-01 08:25:31 -0400

    authentication is safely encrypted

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcryptjs");
+const { match } = require("assert");
 
 const mongoDb = "mongodb+srv://mukeshloganathan12:5I8C6b6XKIenJZ9f@cluster0.xnpubn3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(mongoDb);
@@ -35,7 +36,8 @@ passport.use(
         if (!user) {
           return done(null, false, { message: "Incorrect username" });
         };
-        if (user.password !== password) {
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
           return done(null, false, { message: "Incorrect password" });
         };
         return done(null, user);
@@ -70,18 +72,18 @@ app.get("/", (req, res) => {
   
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 
-app.post("/sign-up", async (req, res, next) => {
+app.post("/sign-up", bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
     try {
-      const user = new User({
-        username: req.body.username,
-        password: req.body.password
-      });
-      const result = await user.save();
-      res.redirect("/");
-    } catch(err) {
-      return next(err);
-    };
-  });
+        const user = new User({
+          username: req.body.username,
+          password: req.body.password
+        });
+        const result = await user.save();
+        res.redirect("/");
+      } catch(err) {
+        return next(err);
+      };
+    }));
   
 app.post(
     "/log-in",
